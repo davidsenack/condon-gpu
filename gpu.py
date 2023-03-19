17dd25a254d70e10f5f9525ed5674869d58a0231
David Senack <david.senack@gmail.com>
2023-03-19 13:34:20 -0400

    Add GPU prime sieve code

---
+++ gpu.py
@@ -0,0 +1,20 @@
+from sys import argv
+import time as t
+
+def is_prime(n):
+    factors = 0
+    for i in range(2, n):
+        if n % i == 0:
+            factors += 1
+    return factors == 0
+
+limit = int(argv[1])
+total = 0
+
+start = t.time()
+@par(schedule='dynamic', chunk_size=100, num_threads=16)
+for i in range(2, limit):
+    if is_prime(i):
+        total += 1
+time = t.time() - start
+print('Found %d primes in %f seconds' % (total, time))
